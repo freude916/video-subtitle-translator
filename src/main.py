7af06715b958ecc7bdb3d8fed78d5d9c88904ab6
zed <2142045058@qq.com>
2025-05-12 11:27:50 +0800

    这下有主动了

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import glob
 import logging
 import os
 from typing import Optional, List, Tuple
@@ -74,10 +75,9 @@ def ocr_lite(frame) -> Optional[Tuple[str, float]]:
             if rv:
                 return rv, c
             elif c < 0.8:
-                return "", 0.7 # 提醒可能存在未出现完全的字幕
+                return "", 0.7  # 提醒可能存在未出现完全的字幕
             else:
                 return None
-
 
 
 def ocr_full(frame) -> Optional[Tuple[str, float]]:
@@ -240,7 +240,7 @@ class VideoDetector:
             test_end += self.frame_gap
         return test_end
 
-    def subtitle_detect(self) -> List[Tuple[int, int, str, float]]:
+    def subtitle_detect(self) -> List[Tuple[int, int, str]]:
         """
         核心逻辑1，执行字幕检测
         :return:
@@ -249,7 +249,7 @@ class VideoDetector:
         current_frame_no = 0  # 正准备扫的帧
         prev_subtitle: str | None = None
 
-        subtitle_result: List[Tuple[int, int, str, float]] = []
+        subtitle_result: List[Tuple[int, int, str]] = []
 
         while self.video.isOpened():
             self.video.set(cv2.CAP_PROP_POS_FRAMES, current_frame_no)
@@ -277,7 +277,7 @@ class VideoDetector:
                 elif current_full_result[1] < 0.8:
                     # 字幕未加载完全
                     prev_frame_no = current_frame_no
-                    current_frame_no += 1 # 少跳一点
+                    current_frame_no += 1  # 少跳一点
                     prev_subtitle = ""
                     continue
                 current_subtitle: str = current_full_result[0]
@@ -289,9 +289,10 @@ class VideoDetector:
                 if current_frame_no > 100:
                     pass
 
-                current_subtitle_end = self.find_subtitle_boundary(jump_end - self.frame_gap, jump_end, current_subtitle)
+                current_subtitle_end = self.find_subtitle_boundary(jump_end - self.frame_gap, jump_end,
+                                                                   current_subtitle)
 
-                subtitle_result.append((current_subtitle_begin, current_subtitle_end, current_subtitle, confidence))
+                subtitle_result.append((current_subtitle_begin, current_subtitle_end, current_subtitle))
 
                 logger.info(f'[{current_subtitle_begin} - {current_subtitle_end}] {current_full_result}')
                 logger.info(f'proceed: {current_frame_no} -> {self.total_frame_count}')
@@ -312,7 +313,9 @@ class VideoDetector:
         self.video.release()
         return subtitle_result
 
-    async def subtitle_translate(self, subtitle: List[Tuple[int, int, str]]) -> List[Tuple[int, int, str]]:
+
+    @staticmethod
+    async def subtitle_translate(subtitle: List[Tuple[int, int, str]]) -> List[Tuple[int, int, str]]:
         """
         核心逻辑2，执行字幕翻译
         :param subtitle:
@@ -379,69 +382,15 @@ class VideoDetector:
         logger.info(f'Merged soft subtitle to {output_file}')
 
 
-subtitle = [(108, 115.5, '盗仓門', 0.5814619660377502), (116.5, 153.0, '盛沧澜', 0.9970487952232361),
-            (154.0, 163.0, '瞧见了吗', 0.9976983070373535), (169.0, 196.5, '那护国公的千金', 0.9897699952125549),
-            (197.5, 223.5, '竟追到了这里', 0.9917173385620117), (225.5, 259.5, '真是不害', 0.9998215436935425),
-            (263.5, 290.5, '是我也不甘心', 0.9988496899604797),
-            (298.5, 345.0, '满心期盼等候的心上', 0.9960343241691589),
-            (346.0, 390.0, '人刚回来就另娶了她人', 0.9953144788742065), (426.0, 439.0, '小姐', 0.9998826384544373),
-            (449.0, 471.5, '我们回去吧', 0.9997744560241699),
-            (479.5, 504.5, '裴将军是不会见我们的', 0.9966943860054016),
-            (516.5, 541.5, '裴将军是不会见我们的', 0.9777539968490601),
-            (542.5, 562.5, '老爷正在找你', 0.9839210510253906), (572.5, 606.5, '若是知道你来了这', 0.9969412088394165),
-            (616.5, 631.5, '恐怕', 0.99873948097229), (677.5, 680.0, '今', 0.7643079161643982),
-            (681.0, 757.5, '日我只想听他亲口说一句', 0.9645965099334717),
-            (785.5, 848.5, '他已经真心爱上了别人', 0.9244571924209595),
-            (868.5, 913.0, '要退婚于我', 0.9957104921340942), (941.0, 943.5, '面', 0.9542027115821838),
-            (997.5, 1036.0, '刀', 0.9594619870185852), (1084.0, 1119.0, '裴某今日大婚', 0.9972534775733948),
-            (1133.0, 1179.5, '婚服尚未来得及更换', 0.9841243028640747),
-            (1185.5, 1234.5, '并听闻盛小姐在此等候', 0.9978328943252563),
-            (1252.5, 1281.0, '有失远迎了', 0.9979439973831177), (1313.0, 1334.0, '裴昭', 0.9949221014976501),
-            (1356.0, 1395.0, '我只问你一次', 0.9983322620391846),
-            (1403.0, 1455.5, '你是否真心退婚于我', 0.9664162397384644), (1459.5, 1462.0, '囍', 0.7538700699806213),
-            (1488.0, 1512.5, '囍', 0.6021003723144531), (1514.5, 1563.5, '我已请得一道退婚圣旨', 0.992054283618927),
-            (1575.5, 1607.0, '你是想抗旨不尊', 0.997049868106842), (1619.0, 1657.5, '被满门抄斩', 0.9710063934326172),
-            (1671.5, 1730.5, '又或者盛姑娘愿意进来', 0.9946429133415222),
-            (1736.5, 1774.0, '给我们作个床伴', 0.9973735213279724),
-            (1798.0, 1841.0, '我倒也不介意', 0.9598219990730286), (1853.0, 1855.5, '囍', 0.5329992771148682),
-            (1967.5, 1988.5, '裴昭', 0.9991567730903625), (2264.5, 2299.0, '那这算什么', 0.9997236132621765),
-            (2501.0, 2556.5, '十年前', 0.995527446269989), (3062.5, 3096.5, '多谢小姐救命之恩', 0.9968143105506897),
-            (3106.5, 3132.5, '你家在哪', 0.9996513724327087), (3133.5, 3172.0, '我让人送你回去', 0.9996028542518616),
-            (3260.0, 3310.0, '家人都已死于战乱', 0.9982414245605469),
-            (3360.0, 3391.0, '我已没有家了', 0.9987406730651855), (3401.0, 3422.5, '既如此', 0.9994029998779297),
-            (3428.5, 3456.5, '那以后便跟着我吧', 0.9990864992141724), (3480.5, 3505.0, '你好好表现', 0.995815098285675),
-            (3506.0, 3543.0, '日后若是获了军功', 0.9974322319030762),
-            (3549.0, 3587.5, '你就可以衣食无忧了', 0.996820867061615),
-            (3763.5, 3808.5, '家父临终有言', 0.9996432662010193), (3809.5, 3848.0, '即便死', 0.9987940788269043),
-            (3854.0, 3898.5, '家传的玉簪也不能丢', 0.9539003372192383),
-            (3920.5, 3951.5, '以后裴昭的命', 0.9984379410743713), (3971.5, 4006.5, '便是小姐的了', 0.9995988011360168),
-            (4302.5, 4350.0, '这支玉簪算什么', 0.9845094680786133),
-            (4370.0, 4427.5, '你的承诺算什么', 0.9995078444480896), (4435.5, 4456.0, '囍', 0.7254359126091003),
-            (4498.0, 4574.0, '我盛沧澜对你而言又算什么', 0.9988043308258057),
-            (4606.0, 4636.5, '自然是算你', 0.9174257516860962), (4656.5, 4698.5, '一厢情愿', 0.9927920699119568),
-            (4762.5, 4801.0, '裴某心上人已有身孕', 0.9976029992103577),
-            (4825.0, 4853.0, '不便被扰', 0.9991797804832458), (4879.0, 4881.5, '9', 0.5140502452850342),
-            (4953.5, 4985.0, '请回吧', 0.9994271397590637), (5013.0, 5015.5, '孝', 0.6377304196357727),
-            (5029.5, 5037.0, '裴昭新', 0.9756647944450378), (5038.0, 5088.5, '宋娇娇', 0.8678662776947021),
-            (5089.5, 5103.5, '裴昭新婚妻子', 0.9576156139373779),
-            (5104.5, 5130.0, '妾身与将军情投意合', 0.9975085854530334),
-            (5134.0, 5169.0, '还望姑娘成全', 0.9987371563911438), (5193.0, 5225.0, '天下姻缘', 0.9727902412414551),
-            (5231.0, 5264.0, '莫要强求为好', 0.9987258315086365),
-            (5460.0, 5528.5, '你说过你的命是我的', 0.9987468719482422),
-            (5560.5, 5622.0, '那你现在便还我', 0.9993332028388977), (5758.0, 5760.5, '勤', 0.6887478232383728)]
-
-
 async def main():
-    print(await detector.subtitle_translate(subtitle))
+    for path in glob.glob("../input/*.*"):
+        detector = VideoDetector(path)
+        subtitle = detector.subtitle_detect()
+        subtitle = await detector.subtitle_translate(subtitle)
+        detector.merge_soft_sub(subtitle)
 
 
-# asyncio.run(main())
+if __name__ == "__main__":
+    import asyncio
 
-if __name__ == '__main__':
-    detector = VideoDetector('/home/zed/Documents/Codes/Python/Subtitle/input/test.mp4')
-    subtitle = detector.subtitle_detect()
-    print(subtitle)
-
-    # detector.subtitle_translate(subtitle)
-
-    # detector.merge_soft_sub(subtitle)
+    asyncio.run(main())
